af00cb3a07a1a6f1dd4383436ca3ddaa6d2d068b
ihsan-afridi <ihsanafridi576284@gmail.com>
2024-07-12 11:51:33 -0700

    js-prectice

---
+++ index.js
@@ -0,0 +1,28 @@
+// JAVASCRIPT PRACTICE PROBLEMS - 2
+
+
+
+// NOTE: HAR PROGRAM KLYE (JAHA ARRAY REQUIRED HA) ARRAY USER SE LE JIS ME 5 ELEMENTS HO
+
+// 6. FIND THE SMALLEST NUMBER IN AN ARRAY
+// var num = [1, 2, 3, 5, 44, 32,]
+// var SmallestaValue = num[0]
+// for(let i = 0; i < num.length; i++){
+//    if (SmallestaValue > num[i]) {
+//      SmallestaValue = num[i]
+//    }
+// }
+// console.log({SmallestaValue: SmallestaValue});
+
+
+// 7. CONVERT CELCIUS TO FAHRENHEIT
+
+// 8. CONVERT FAHRENHEIT TO CELCIUS
+
+// 9. REVERSE AN ARRAY
+
+// 10. CALCULATE AVERAGE OF AN ARRAY
+
+
+
+console.log("Apple" < "Banana");
